154e115d18334562dcc7c3fa946cce5e815344d7
Fazreilie Alep <fazreilie122@gmail.com>
2025-02-01 18:42:29 +0800

    add records to db and random anime fetch

--- scripts/animelistapi.py
+++ scripts/animelistapi.py
@@ -2,6 +2,7 @@ import http.client
 import json
 import pandas as pd
 import ast
+import os
 
 
 """ FUNCTIONS """
@@ -19,6 +20,7 @@ def convert_to_json(string_list):
 # Transforming the data to fit the AnimeCreate ReqBody model
 def transform_data(data):
     transformed_data = {
+        "myanimelist_id": data.get("myanimelist_id"),
         "title_ov": data.get("title_ov"),
         "title_en": data.get("title_en"),
         "synopsis": data.get("synopsis"),
@@ -68,14 +70,19 @@ def transform_data(data):
     Ensure to update headers, toJSONfile
 
 """
-folder = 'data/MyAnimeListB2'
-toReJSONfile = folder + '/ReMyAnimeList.json'
-toJSONfile = folder + '/MyAnimeList.json'
+folder = 'data/MyAnimeListB3'
+toReJSONfile = folder + '/ReMyAnimeList2.json'
+toJSONfile = folder + '/MyAnimeList2.json'
+
+# Create the directory if it doesn't exist
+os.makedirs(folder, exist_ok=True)
+
 # fromCSVfile = 'data/MyAnimeList_V2.csv'
 # df = pd.read_csv(fromCSVfile)  # Replace with your file path
 # id_array = df['ID'].to_numpy()
 
-id_array = [37055, 32615, 39587, 36286, 31240, 54857, 42203, 13759, 52578]
+# id_array = [52481, 33206, 35363, 39247, 36793, 37956, 46569, 53126, 50593, 35851, 41361, 38816, 52034, 55791, 57334, 56843, 55830, 53127, 34321, 38084, 38085, 38086, 41497, 40787, 37171, 9989, 15039, 53393, 57524, 37430, 39551, 41487, 53580, 28851, 18671, 14741,35608, 38101, 39783, 48548, 58755, 55887, 58939, 53802, 6045, 20847, 10119, 26123, 30240, 58426, 54968, 6547, 38329, 53129, 54870, 57433, 5081, 110031, 54857]
+id_array = [47194, 55830]
 
 # Define the connection and headers
 conn = http.client.HTTPSConnection("myanimelist.p.rapidapi.com")
@@ -96,6 +103,7 @@ anime_data_list = []
 for anime_id in id_array:
     try:
         anime_data = fetch_anime_data(str(anime_id))
+        anime_data['myanimelist_id'] = anime_id
         anime_data_list.append(str(anime_data))
         print('ok ' + str(anime_id))
     except Exception as e:
@@ -106,6 +114,7 @@ conn.close()
 
 """ converting json string to json"""
 json_data = convert_to_json(anime_data_list)
+print(json_data)
 
 # Write json_data to the output file
 with open(toJSONfile, "w") as output_file:
